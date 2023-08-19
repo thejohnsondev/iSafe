2e42eb07c363c36e6f972178cb5ab08ece828a2a
Andrew Dobosh <thejohnsondev@gmail.com>
2023-08-19 10:30:15 +0300

    Change animations to fade animations

--- app/src/main/java/com/thejohnsondev/isafe/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/thejohnsondev/isafe/presentation/navigation/Navigation.kt
@@ -1,8 +1,9 @@
 package com.thejohnsondev.isafe.presentation.navigation
 
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.navigation.animation.AnimatedNavHost
@@ -15,6 +16,7 @@ import com.thejohnsondev.isafe.presentation.screens.login.LoginScreen
 import com.thejohnsondev.isafe.presentation.screens.login.LoginViewModel
 import com.thejohnsondev.isafe.presentation.screens.signup.SignUpScreen
 import com.thejohnsondev.isafe.presentation.screens.signup.SignUpViewModel
+import com.thejohnsondev.isafe.utils.TWEEN_ANIM_DEFAULT_DURATION
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -24,40 +26,32 @@ fun Navigation() {
         navController = navController,
         startDestination = Screens.CheckAuthScreen.name
     ) {
-        composable(route = Screens.CheckAuthScreen.name) {
+        composable(
+            route = Screens.CheckAuthScreen.name,
+            enterTransition = null,
+            popEnterTransition = null
+        ) {
             val viewModel = hiltViewModel<CheckAuthStateViewModel>()
             CheckAuthStateScreen(navController = navController, viewModel = viewModel)
         }
         composable(route = Screens.LoginScreen.name,
             enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentScope.SlideDirection.Companion.Left,
-                    animationSpec = tween(350)
-                )
+                fadeIn(animationSpec = tween(TWEEN_ANIM_DEFAULT_DURATION))
             },
             exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentScope.SlideDirection.Companion.Right,
-                    animationSpec = tween(350)
-                )
-            },
-            popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentScope.SlideDirection.Companion.Left,
-                    animationSpec = tween(350)
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentScope.SlideDirection.Companion.Right,
-                    animationSpec = tween(350)
-                )
+                fadeOut(animationSpec = tween(TWEEN_ANIM_DEFAULT_DURATION))
             }) {
             val viewModel = hiltViewModel<LoginViewModel>()
             LoginScreen(navController = navController, viewModel = viewModel)
         }
         composable(
-            route = Screens.SignUpScreen.name
+            route = Screens.SignUpScreen.name,
+            enterTransition = {
+                fadeIn(animationSpec = tween(TWEEN_ANIM_DEFAULT_DURATION))
+            },
+            exitTransition = {
+                fadeOut(animationSpec = tween(TWEEN_ANIM_DEFAULT_DURATION))
+            }
         ) {
             val viewModel = hiltViewModel<SignUpViewModel>()
             SignUpScreen(navController = navController, viewModel = viewModel)
--- app/src/main/java/com/thejohnsondev/isafe/utils/Constants.kt
+++ app/src/main/java/com/thejohnsondev/isafe/utils/Constants.kt
@@ -6,3 +6,5 @@ const val DEFAULT_USER_ID = "default_user_id"
 const val DEFAULT_USER_NAME = "User"
 
 const val EMPTY = ""
+
+const val TWEEN_ANIM_DEFAULT_DURATION = 300
