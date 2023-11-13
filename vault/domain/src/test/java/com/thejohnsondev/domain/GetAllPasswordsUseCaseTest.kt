37ff95b684e9fe940076a12a21a15eff7637f29b
Andrew <thejohnsondev@gmail.com>
2023-11-13 18:08:40 +0200

    Update test cases for getting all passwords and deleting passwords use cases, use fake repository

--- vault/domain/src/test/java/com/thejohnsondev/domain/GetAllPasswordsUseCaseTest.kt
+++ vault/domain/src/test/java/com/thejohnsondev/domain/GetAllPasswordsUseCaseTest.kt
@@ -2,17 +2,14 @@ package com.thejohnsondev.domain
 
 import com.google.common.truth.Truth.assertThat
 import com.thejohnsondev.data.PasswordsRepository
-import com.thejohnsondev.model.PasswordModel
+import com.thejohnsondev.data.test.FakePasswordRepository
 import com.thejohnsondev.model.UserPasswordsResponse
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.BDDMockito.given
-import org.mockito.Mock
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.junit.MockitoRule
@@ -20,7 +17,6 @@ import org.mockito.junit.MockitoRule
 @RunWith(MockitoJUnitRunner::class)
 class GetAllPasswordsUseCaseTest {
 
-    @Mock
     private lateinit var passwordsRepository: PasswordsRepository
     private lateinit var getAllPasswordsUseCase: GetAllPasswordsUseCaseImpl
 
@@ -29,43 +25,24 @@ class GetAllPasswordsUseCaseTest {
 
     @Before
     fun setUp() {
+        passwordsRepository = FakePasswordRepository()
         getAllPasswordsUseCase = GetAllPasswordsUseCaseImpl(passwordsRepository)
     }
 
     @Test
     fun givenValidUserId_returnsSuccess() = runBlocking {
-        val correctFlowToReturn = flowOf(UserPasswordsResponse.ResponseSuccess(emptyList()))
-        given(passwordsRepository.getUserPasswords("id")).willReturn(correctFlowToReturn)
+        val result = getAllPasswordsUseCase("1").first()
 
-        val result = getAllPasswordsUseCase("id")
-
-        assertThat(result).isEqualTo(correctFlowToReturn)
+        assertThat(result).isInstanceOf(UserPasswordsResponse.ResponseSuccess::class.java)
     }
 
     @Test
     fun givenValidUserId_returnsValidList() = runBlocking {
-        val correctFlowToReturn = flowOf(
-            UserPasswordsResponse.ResponseSuccess(
-                listOf(
-                    PasswordModel(
-                        id = "id",
-                        organization = "org",
-                        title = "title",
-                        password = "pass"
-                    )
-                )
-            )
-        )
-        given(passwordsRepository.getUserPasswords("id")).willReturn(correctFlowToReturn)
+        val result = getAllPasswordsUseCase("1").first()
 
-        val result = getAllPasswordsUseCase("id").first()
-
-        assertThat(result).isInstanceOf(UserPasswordsResponse.ResponseSuccess::class.java)
         if (result is UserPasswordsResponse.ResponseSuccess) {
-            assertThat(result.passwords.first().id == "id"
-                    && result.passwords.first().organization == "org"
-                    && result.passwords.first().title == "title"
-                    && result.passwords.first().password == "pass")
+            assertThat(result.passwords.first()).isEqualTo(FakePasswordRepository.user1TestPasswords.first())
+            assertThat(result.passwords.last()).isEqualTo(FakePasswordRepository.user1TestPasswords.last())
         }
     }
 
