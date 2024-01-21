609364f04960b80dbe410621a0c68381fabe21f3
Andrew <thejohnsondev@gmail.com>
2024-01-21 13:03:39 +0200

    Version bump 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.thejohnsondev.isafe"
         minSdk = 29
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
