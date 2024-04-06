f33f3a6b15e1b09e2a39963b60bf4d6b67d518cc
Andrew <thejohnsondev@gmail.com>
2024-04-06 19:22:08 +0300

    Fix scrolling on add edit password screen

--- passwordaddedit/presentation/src/main/java/com/thejohnsondev/presentation/AddEditPasswordScreen.kt
+++ passwordaddedit/presentation/src/main/java/com/thejohnsondev/presentation/AddEditPasswordScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -207,6 +208,7 @@ fun AddEditPasswordContent(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .imePadding()
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Top,
