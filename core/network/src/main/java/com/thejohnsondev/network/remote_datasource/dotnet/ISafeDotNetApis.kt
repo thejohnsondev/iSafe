c11b35d32dd8923f3fea1b110cfb2ea2dc38fa38
Andrew <thejohnsondev@gmail.com>
2024-06-13 17:48:14 +0300

    Update API for GetNote

--- core/network/src/main/java/com/thejohnsondev/network/remote_datasource/dotnet/ISafeDotNetApis.kt
+++ core/network/src/main/java/com/thejohnsondev/network/remote_datasource/dotnet/ISafeDotNetApis.kt
@@ -4,7 +4,7 @@ const val SIGN_UP = "Auth/Register"
 const val LOGIN = "Auth/Login"
 const val DELETE_ACCOUNT = "Auth/DeleteAllData"
 
-const val GET_PASSWORDS = "Password/GetPassword"
+const val GET_PASSWORDS = "Password/GetPasswords"
 const val ADD_PASSWORD = "Password/AddPassword"
 const val UPDATE_PASSWORD = "Password/UpdatePassword"
 const val DELETE_PASSWORD = "Password/DeletePassword"
