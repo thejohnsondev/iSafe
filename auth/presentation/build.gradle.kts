718afb39d21466835f9c057717eafbe585bf24d6
Andrew <thejohnsondev@gmail.com>
2024-07-20 11:06:56 +0300

    Update compose navigation library

--- auth/presentation/build.gradle.kts
+++ auth/presentation/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
 
     // Hilt-Dagger
     implementation(libs.com.google.dagger.hilt.android)
