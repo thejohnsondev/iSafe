324215ecce1faecbccd60a02ad1ab5ea5f95019c
Andrew <thejohnsondev@gmail.com>
2024-09-18 14:03:05 +0300

    Fix running tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.2")
+        classpath(libs.google.services)
     }
 }
 plugins {
@@ -13,7 +13,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.android.library") version "8.1.4" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
     kotlin("kapt") version "1.7.20"
