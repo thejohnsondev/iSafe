d8ff9d968986bba0697d5a7a36ab01d40820cfc2
Andrew <thejohnsondev@gmail.com>
2024-10-15 17:20:44 +0300

    Add deploy to app distribution job

--- auth/data/build.gradle.kts
+++ auth/data/build.gradle.kts
@@ -18,7 +18,7 @@ val authSecretIV: String? = gradleLocalProperties(rootDir).getProperty("auth_sec
 
 android {
     namespace = "com.thejohnsondev.data"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 29
--- notes/data/build.gradle.kts
+++ notes/data/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.thejohnsondev.data"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 29
